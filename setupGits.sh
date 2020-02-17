d8df9a1368fc7970226b5e055963d5e8a825e9cd
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2020-02-17 12:06:10 +0100

    Creation structure dossiers

--- setupGits.sh
+++ setupGits.sh
@@ -10,7 +10,8 @@ git add .
 git commit -m "Creation structure dossiers"
 
 # For every group, add the corresponding remote and push
-for i in $(seq -f "%02g" 4 1 $MAX_GROUPS)
+for i in $(seq -f "%02g" 1 1 3)
+#for i in $(seq -f "%02g" 1 1 $MAX_GROUPS)
 do
    git remote add Groupe$i https://gitlab.com/infof307-1920/groupe$i.git
    git push Groupe$i master
