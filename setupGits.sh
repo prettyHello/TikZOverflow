5b5c12020279bd73530d2ed2626041f25c13cd8f
Jacopo De Stefani <jacopo.de.stefani@gmail.com>
2020-02-17 12:02:53 +0100

    Creation structure dossiers

---
+++ setupGits.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MAX_GROUPS=13
+
+# Create empty git repository
+git init
+
+# Add all the files in the template directory to Git
+git add .
+git commit -m "Creation structure dossiers"
+
+# For every group, add the corresponding remote and push
+for i in $(seq -f "%02g" 4 1 $MAX_GROUPS)
+do
+   git remote add Groupe$i https://gitlab.com/infof307-1920/groupe$i.git
+   git push Groupe$i master
+done
