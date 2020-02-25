375bfc88669416f92bb70c3b0e48b1400f46caef
Pablo Rey Pedrosa <pablo.reypedrosa@gmail.com>
2020-02-25 12:19:58 +0100

    Email check

--- src/view/registration/RegisterController.java
+++ src/view/registration/RegisterController.java
@@ -43,27 +43,31 @@ public class RegisterController {
     String passwordText;
 
     private String unallowedCharactersPattern = "[\\\\|@#~€¬\\[\\]{}!\"·$%&\\/()=?¿^*¨;:_`\\+´,.-]";
+
     private String whitespacesPattern = "^[\\s]+|[\\s]+$";
+
+    private String emailPattern = "(?:[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+\\/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
 
     public void registerBtn(){
 
 
-        UserFactoryImpl dto = new UserFactoryImpl();
+        /*UserFactoryImpl dto = new UserFactoryImpl();
         UserDTO user = dto.createUser(0,"firstname","lastname","email2","ring ring2","password","random blob",Utility.getTimeStamp());
         DALServices dal = new DALServicesImpl();
         UserDAOImpl dao = new UserDAOImpl(dal,dto);
         UserUCC userUcc = new UserUCCImpl(dal,dao);
-        userUcc.register(user);
+        userUcc.register(user);*/
 
         System.out.println("Register");
 
         this.checkFirstName();
         this.checkLastName();
+        this.checkEmail();
     }
 
     public boolean checkFirstName() {
-
-        if (firstnameTF.getText().matches(this.unallowedCharactersPattern))
+        System.out.println("Checking firstname");
+        if (this.firstnameTF.getText().isEmpty() || this.firstnameTF.getText().matches(this.unallowedCharactersPattern))
             return false;
         else
             // Remove whitespaces at the beginning and at the end
@@ -74,8 +78,8 @@ public class RegisterController {
     }
 
     public boolean checkLastName() {
-
-        if (lastnameTF.getText().matches(this.unallowedCharactersPattern))
+        System.out.println("Checking lastname");
+        if (this.lastnameTF.getText().isEmpty() || this.lastnameTF.getText().matches(this.unallowedCharactersPattern))
             return false;
         else
             // Remove whitespaces at the beginning and at the end
@@ -84,7 +88,16 @@ public class RegisterController {
         return true;
     }
 
-    public boolean checkEmail() { System.out.println("Register"); return false; }
+    public boolean checkEmail() {
+        System.out.println("Checking email");
+        if (this.emailTF.getText().matches(this.emailPattern)) {
+            this.emailText = this.emailTF.getText();
+            return true;
+        }
+
+        return false;
+
+    }
 
     public boolean checkPhone() { System.out.println("Register"); return false; }
 }
