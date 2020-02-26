df49468c044b7a0b92afea177c43c794879ae809
Olivier De Canniere <olivier.decanniere96@gmail.com>
2020-02-26 20:14:41 +0100

    minor ui tweaks

--- src/view/login/LoginController.java
+++ src/view/login/LoginController.java
@@ -3,6 +3,7 @@ package view.login;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import view.ViewName;
 import view.ViewSwitcher;
@@ -12,7 +13,7 @@ public class LoginController {
     public TextField tf_username;
 
     @FXML
-    public TextField tf_password;
+    public PasswordField tf_password;
 
     @FXML
     public Button bt_login;
