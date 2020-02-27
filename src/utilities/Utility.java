b4a2e2b5eca052d34396a7f07a1d7a926e53f759
Anicet siewe <asieweko@ulb.ac.be>
2020-02-27 03:36:38 +0100

    add unTarFile() function

--- src/utilities/Utility.java
+++ src/utilities/Utility.java
@@ -1,9 +1,15 @@
 package utilities;
 
 import javafx.scene.Scene;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.utils.IOUtils;
 
+import java.io.*;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.zip.GZIPInputStream;
 
 public final class Utility {
 
@@ -15,6 +21,36 @@ public final class Utility {
         /*System.out.println(formatter.format(date));*/
         return formatter.format(date);
 
+    }
+
+    public static void unTarFile(File tarFile, Path destFile)
+    {
+        TarArchiveInputStream tis = null;
+        try {
+            FileInputStream fis = new FileInputStream(tarFile);
+            GZIPInputStream gzipInputStream = new GZIPInputStream(new BufferedInputStream(fis));
+            tis = new TarArchiveInputStream(gzipInputStream);
+            TarArchiveEntry tarEntry = null;
+            while ((tarEntry = tis.getNextTarEntry()) != null)
+            {
+                if(tarEntry.isDirectory())
+                {
+                    continue;
+                }else {
+                    File outputFile = new File(destFile.toString() + File.separator + tarEntry.getName());
+                    outputFile.getParentFile().mkdirs();
+                    IOUtils.copy(tis, new FileOutputStream(outputFile));
+                }
+            }
+        }catch(IOException ex) {
+            System.out.println("Error while untarring a file- " + ex.getMessage());
+        }finally { if(tis != null) { try { tis.close(); } catch (IOException e) { e.printStackTrace(); } } }
+    }
+
+
+    public static String HachFonction (String name) {
+
+        return name ;
     }
 
 
