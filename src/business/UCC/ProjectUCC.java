d42f9c9d72a218243fe4ace7d141269c715b23ca
Elbigodo <rbigodon@ulb.ac.be>
2020-03-06 20:29:26 +0100

    add javaDoc

--- src/business/UCC/ProjectUCC.java
+++ src/business/UCC/ProjectUCC.java
@@ -9,10 +9,29 @@ import java.io.File;
 import java.nio.file.Path;
 
 public interface ProjectUCC {
+    /**
+     * Rename imported project with input provided by the user
+     * @param projectName  folder to be rename
+     * @param NewProjectName new folder name
+     */
 
     public void renameFolderProject(File projectName, File NewProjectName);
 
+    /**
+     * this setter retrieves the project name from the user thank to the dialog box
+     * @param popupMessage  this variable contents the instruction showed to the user
+     * @return return the project name typed by the user
+     */
+
     public String setProjectName (String popupMessage) throws BizzException;
+
+    /**
+     *  retrieve information for a given project and user
+     * @param projectName  project name
+     * @param folderDestination path to the project folder
+     * @param userId  project owner
+     * @return
+     */
 
     public ProjectDTO getProjectDTO(String projectName, Path folderDestination, int userId);
 
