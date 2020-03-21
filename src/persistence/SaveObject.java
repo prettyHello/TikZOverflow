dd0122c5ba5030d82b0316fbe0758f3e7571dc7d
adrenbe <adrenbe@protonmail.com>
2020-03-21 16:15:14 +0100

    Merge branch 'savingObject' of https://gitlab.com/infof307-1920/groupe09 into savingObject
    # Conflicts:
    #	src/persistence/SaveObject.java

--- src/persistence/SaveObject.java
+++ src/persistence/SaveObject.java
@@ -1,16 +1,40 @@
 package persistence;
 
+import business.Canvas.Shape;
+import business.DTO.ProjectDTO;
+import business.UCC.ProjectUCCImpl;
+
 import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 
 public class SaveObject {
+    private String rootProject = File.separator + "ProjectTikZ" + File.separator;
+    // Saving the magnificient drawing of the users into a file
+    public void save(List<Shape> myList, String nameOfTheFile) throws IOException, ClassNotFoundException {
+        //Iterating into the list of shapes
+        for (Shape shape : myList) {
+            //Creating an object to save into the file
+            ObjectOutputStream objectOutputStream =
+                    new ObjectOutputStream(new FileOutputStream(rootProject + nameOfTheFile + ".bin"));
+            objectOutputStream.writeObject(shape);
+            objectOutputStream.close();
+        }
+    }
+    public List <Shape> open(String nameOfTheFile) throws IOException, ClassNotFoundException {
+        List myList = null;
+        // We still
 
-    public < E > void save( E inputObject ) throws IOException, ClassNotFoundException {
-        // Save elements
-        ObjectOutputStream objectOutputStream =
-                new ObjectOutputStream(new FileOutputStream("data/person.bin"));
-        objectOutputStream.writeObject(inputObject);
-        objectOutputStream.close();
+        ObjectInputStream objectInputStream =
+                new ObjectInputStream(new FileInputStream(nameOfTheFile));
+
+        Shape shape = (Shape) objectInputStream.readObject();
+
+        objectInputStream.close();
+
+        return myList;
     }
 
 }
