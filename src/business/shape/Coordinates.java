da78b85182ffd2a5750603cc74872349c56fbfe7
adrenbe <adrenbe@protonmail.com>
2020-03-24 15:01:31 +0100

    Saving function is nearly done (it is working) just need to be cleaned up and to be linked with the others classes.

--- src/business/shape/Coordinates.java
+++ src/business/shape/Coordinates.java
@@ -1,12 +1,14 @@
 package business.shape;
 
+import java.io.Serializable;
+
 /**
  * Pair of coordinates, with their respective units and options.
  * only work with default units for now (cm).
  * https://en.wikibooks.org/wiki/LaTeX/PGF/TikZ#Specifying_Coordinates
  *
  */
-public class Coordinates {
+public class Coordinates implements Serializable {
     private float x = 0;
     private float y = 0;
 
--- src/persistence/SaveObject.java
+++ src/persistence/SaveObject.java
@@ -1,6 +1,7 @@
 package persistence;
 
 import business.Canvas.Canvas;
+import business.Canvas.CanvasImpl;
 import business.shape.Shape;
 import business.DTO.ProjectDTO;
 import business.UCC.ProjectUCCImpl;
@@ -11,19 +12,30 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class SaveObject {
-    private String rootProject = File.separator + "ProjectTikZ" + File.separator;
+    private String rootProject = System.getProperty("user.home") + File.separator + "ProjectTikZ" + File.separator;
     // Saving the magnificient drawing of the users into a file
-    public void save(Canvas canvas, String nameOfTheFile) throws IOException, ClassNotFoundException {
+    public void save(CanvasImpl canvas, String nameOfTheFile) throws IOException, ClassNotFoundException {
         //Iterating into the list of shapes
         ObjectOutputStream objectOutputStream =
                 new ObjectOutputStream(new FileOutputStream(rootProject + nameOfTheFile + ".bin"));
 
-            objectOutputStream.writeObject(canvas);
-            objectOutputStream.close();
-            System.out.println("Project saved successfully");
+        objectOutputStream.writeObject(canvas);
+        objectOutputStream.close();
+        System.out.println("Project saved successfully");
+
+
+        FileInputStream fileInputStream = new FileInputStream(rootProject + nameOfTheFile + ".bin");
+        ObjectInputStream in = new ObjectInputStream(fileInputStream);
+        CanvasImpl readCanvas = (CanvasImpl) in.readObject();
+
+        in.close();
+        ArrayList<Shape> shapes = (ArrayList<Shape>) readCanvas.getShapes();
+        for (Shape x : shapes){
+            System.out.println(x.print());
+        }
     }
+    //Method still has to change
     public List <Shape> open(String nameOfTheFile) throws IOException, ClassNotFoundException {
         ArrayList myList = null;
         // We still need to work here
