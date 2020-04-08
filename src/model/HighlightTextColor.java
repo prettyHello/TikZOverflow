e8be6fbe02a66478318e3632443327f3d5a73328
Anicet siewe <asieweko@ulb.ac.be>
2020-04-08 18:21:33 +0200

    syntax highlighting model

---
+++ src/model/HighlightTextColor.java
@@ -0,0 +1,124 @@
+package model;
+import controller.Canvas.ActiveCanvas;
+import javafx.scene.input.KeyCode;
+import javafx.scene.shape.Shape;
+import org.fxmisc.richtext.CodeArea;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class HighlightTextColor  extends CodeArea{
+
+    private ArrayList<Shape> selectedShapes = new ArrayList<>();
+
+    public HighlightTextColor setSelectedShapes(ArrayList<Shape> selectedShapes) {
+        this.selectedShapes = selectedShapes;
+        return  this;
+    }
+
+    public HighlightTextColor() {
+        super();
+       keyValidation();
+    }
+
+    public void colorWord(String wordListOption, String color) {
+        String[] wordList = getWordList(wordListOption);
+        for(int x = 0; x < wordList.length; x ++){
+            List<Integer> positions = findWordUpgrade(this.getText(), wordList[x]);
+            for(int y = 0; y < positions.size(); y++){
+                this.setStyleClass(positions.get(y), positions.get(y)+ wordList[x].length(), color);
+            }
+        }
+    }
+
+    private String[] getWordList(String wordListOption) {
+        String[] keyWord = {"filldraw","draw","path","node","begin","tikzstyle","fill","end"}; // Words use by tikz and that will be highlight.
+        String[] keyCharacter = {"\\", ",", "[", "]", "(", ")"} ;
+        String[] wordList=null;
+
+        if(wordListOption == "keyWord"){
+            wordList=  keyWord ;
+        } else  if (wordListOption == "keyCharacter"){
+            wordList=  keyCharacter ;
+        }
+        return wordList;
+    }
+
+    /**
+     * Return the positions of each given word in a string
+     */
+    private List<Integer> findWordUpgrade(String textString, String word) {
+        List<Integer> indexes = new ArrayList<Integer>();
+        StringBuilder output = new StringBuilder();
+        String lowerCaseTextString = textString.toLowerCase();
+        String lowerCaseWord = word.toLowerCase();
+        int wordLength = 0;
+
+        int index = 0;
+        while(index != -1){
+            index = lowerCaseTextString.indexOf(lowerCaseWord, index + wordLength);  // Slight improvement
+            if (index != -1) {
+                indexes.add(index);
+            }
+            wordLength = word.length();
+        }
+        return indexes;
+    }
+
+    /**
+     * Apply a regex on a string
+     */
+    private static String printMatches(String text, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        // Check all occurrences
+        while (matcher.find()) {
+            break;
+        }
+        return matcher.group();
+    }
+
+    public void highlightOnSelect(){
+        if(!getText().isEmpty()){ // Check if the canvas isn't empty (for example at start) Because if the canvas is empty it will make an error.
+            colorWord("keyWord", "blue");
+            colorWord("keyCharacter", "chara");
+        }
+
+        if(!selectedShapes.isEmpty()) // If user select a shape.
+        {
+            for(int z = 0 ; z < selectedShapes.size(); z ++) // Loop for every shape selected.
+            {
+                String myShape = selectedShapes.get(selectedShapes.size() - (z+1)).toString(); // Selection of the actual selected shape.
+                int id = Integer.parseInt(printMatches(myShape,"(id=)[0-9]*").substring(3)); // Take the id of the shape from the var selectedShapes.
+                controller.shape.Shape mySelectedShape = ActiveCanvas.getActiveCanvas().getShapeById(id);   // Take the shape with the id.
+                List<Integer> positions = findWordUpgrade(ActiveCanvas.getActiveCanvas().toTikZ(), mySelectedShape.print()); // Get the line position in the canvas of the selected shape.
+                for(int y = 0; y < positions.size(); y++){
+                    setStyleClass(positions.get(y), positions.get(y)+mySelectedShape.print().length(), "highlight"); // Highlight the position of the shape.
+                }
+            }
+        }
+    }
+
+    private void keyValidation() {
+        setOnKeyReleased(key ->
+        {
+            if (key.getCode().isDigitKey() ||
+                    key.getCode().isFunctionKey() ||
+                    key.getCode().isArrowKey() ||
+                    key.getCode().isKeypadKey() ||
+                    key.getCode().isLetterKey() ||
+                    key.getCode().isMediaKey() ||
+                    key.getCode().isModifierKey() ||
+                    key.getCode().isNavigationKey() ||
+                    key.getCode().isWhitespaceKey() ||
+                    key.getCode() == KeyCode.BACK_SPACE )
+            {
+                setStyleClass(0, getText().length(), "black");
+                colorWord("keyWord", "blue");
+                colorWord("keyCharacter", "chara");
+            }
+        });
+    }
+}
